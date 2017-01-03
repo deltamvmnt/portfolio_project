8c71f86b9b64c8246f32f608ff2cc9e3df7baefb
Michael Neilson <michaelshawnneilson@gmail.com>
2017-01-03 05:29:02 +0000

    Improved error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to new_contact_path, notice: "Message received, thanks!"
+      flash[:success] = "Message received"
+      redirect_to new_contact_path
     else
-      redirect_to new_contact_path, notice: "Oh man, something went wrong. Remember, everything needs to be filled out!"
+      flash[:error] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
     end
   end
   
