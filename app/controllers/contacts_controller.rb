b3ef2a634f07506bf8b83847774ee9c86134b6df
Michael Neilson <michaelshawnneilson@gmail.com>
2017-01-03 05:06:27 +0000

    Added form validations to model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     if @contact.save
       redirect_to new_contact_path, notice: "Message received, thanks!"
     else
-      redirect_to new_contact_path, notice: "Oh man, something isn't right"
+      redirect_to new_contact_path, notice: "Oh man, something went wrong. Remember, everything needs to be filled out!"
     end
   end
   
