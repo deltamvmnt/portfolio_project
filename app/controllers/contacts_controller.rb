6390644c4736f73be8e5551041345768c32d886a
Michael Neilson <michaelshawnneilson@gmail.com>
2017-01-03 18:20:47 +0000

    Added custom URL to page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      flash[:warning] = "Message received"
+      flash[:warning] = "Message received, thanks!"
       redirect_to new_contact_path
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
