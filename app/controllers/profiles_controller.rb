ad49369ecde643b5f4ef43b8b4abedfe5d681fbc
Michael Neilson <michaelshawnneilson@gmail.com>
2017-01-13 20:12:03 +0000

    Added before filters to authenticate users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :only_current_user
   
   # GET to /users/:user_id/profile_new
   def new
@@ -26,8 +27,28 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  # PUT to /users/:user_id/profile
+  def update
+    # Retrieve the user from the db
+    @user = User.find( params[:user_id] )
+    # Retrieve the user's profile info
+    @profile = @user.profile
+    # Mass assign edited in whitelisted profile attributes
+    if @profile.update_attributes(profile_params)
+      flash[:warning] = "Profile has been updated"
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(ryematch_url) unless @user == current_user
     end
 end
